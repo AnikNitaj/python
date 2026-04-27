89053ec7b73f967660ab06e34597731a42aa1a2f
unknown <Student@DIGITALSCHOOL.LOCAL>
2026-04-27 19:20:06 +0200

    lesson1

---
+++ Moduli1/lesson1.py
@@ -0,0 +1,19 @@
+name ="donjeta"
+print(name)
+
+x = 7
+y = 4
+
+print(x-y)
+
+numer="10"
+konvertimi=int(numer)
+
+print(konvertimi)
+print(type(x))
+
+print(type(name))
+
+numriFloat= 10.5
+
+print(numriFloat)
